1eab8fbb39e84d32540523bf67794fc413084060
12345exe <dimon99312@gmail.com>
2026-04-01 00:54:46 +0300

    Update players.cpp

--- src/game/client/components/players.cpp
+++ src/game/client/components/players.cpp
@@ -2,6 +2,7 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 
 #include "players.h"
+#include <game/client/gameclient.h>
 
 #include <base/color.h>
 #include <base/math.h>
